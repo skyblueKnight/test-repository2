d782d1a9d030158437e8bfc27bba0f10e5997a1a
momoyo.kanie <momoyo.kanie@rakus.co.jp>
2019-06-18 14:27:41 +0900

    プロジェクトを作成しました。

---
+++ src/main/java/com/example/TestRepository2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepository2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepository2Application.class, args);
+	}
+
+}
